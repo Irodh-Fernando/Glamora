295b0a932af04dbff9ad025583bba916eade891a
Irodh-Fernando <irodhfernando1@gmail.com>
2025-07-23 22:15:54 +0530

    feat: implement user login with bcrypt validation and JWT token 🔐

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export function createUser(req,res){
     const passwordHash = bcrypt.hashSync(req.body.password, 10)
@@ -22,4 +23,44 @@ export function createUser(req,res){
             message : "Failed to create user"
         })
     })
+}
+
+export function loginUser(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({
+        email: email
+    }).then(
+        (user)=>{
+            if(user==null){
+                res.status(404).json({
+                    message : "User not found"
+                })
+            }else{
+                const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+                if(isPasswordCorrect){
+                    const token = jwt.sign({
+                        firstName : user.firstName,
+                        lastName : user.lastName,
+                        email : user.email,
+                        role : user.role,
+                        isBlocked : user.isBlocked,
+                        isEmailVerified : user.isEmailVerified,
+                        image : user.image
+                    },process.env.JWT_TOKEN_KEY)
+
+                    res.json({
+                        token : token,
+                        message : "Login Successful !"
+                    })
+
+                }else{
+                    res.status(403).json({
+                        message : "Incorrect Password"
+                    })
+                }
+            }
+        }
+    )
 }

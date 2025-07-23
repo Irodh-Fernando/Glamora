662af2dc82e845c4ebe5ddbfe9bf518bd016d47b
Irodh-Fernando <irodhfernando1@gmail.com>
2025-07-23 14:34:53 +0530

    chore: Connect MongoDB using Mongoose 🔌

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
+
+const connectionString = process.env.MONGO_URI;
+mongoose.connect(connectionString).then(()=>{
+    console.log("Connected to the Database");
+}).catch(()=>{
+    console.log("Failed to connect to the Database");
+})
 
 //Start server
 const PORT = 5000;

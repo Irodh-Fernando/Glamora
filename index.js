1fd6737badc3f0d283c1822e61842f88b40a1eb8
Irodh-Fernando <irodhfernando1@gmail.com>
2025-07-24 01:03:49 +0530

    🛡️ feat: add JWT auth middleware to verify Authorization token

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import userRouter from "./routers/userRouter";
+import userRouter from "./routers/userRouter.js";
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+    (req,res,next)=>{
+        const value = req.header("Authorization")
+        if(value != null){
+            const token = value.replace("Bearer ","")
+            jwt.verify(token,process.env.JWT_TOKEN_KEY,(err,decoded)=>{
+                if(decoded == null){
+                    res.status(403).json({
+                        message : "Unauthorized"
+                    })
+                }else{
+                    req.user = decoded
+                    next()
+                }
+            })
+        }else{
+            next()
+        }
+    }
+)
 
 const connectionString = process.env.MONGO_URI;
 mongoose.connect(connectionString).then(()=>{
